597fe620a95b50b0c74db0cd7e332ce7eb11abe7
Anushiya <anushiyadhamu@gmail.com>
2023-07-26 16:03:05 +0530

    added another logger

--- src/main/java/com/zaga/VendorProj/VendorService.java
+++ src/main/java/com/zaga/VendorProj/VendorService.java
@@ -1,20 +1,35 @@
 package com.zaga.VendorProj;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class VendorService {
 
-    private static final String EXTERNAL_API_URL = "http://localhost:5089/orders/getOrders";
+    private static final Logger logger = LogManager.getLogger(VendorService.class);
+
+    @Autowired
+    Config appConfig;
 
     private final RestTemplate restTemplate;
 
-    public VendorService(RestTemplate restTemplate) {
+    public VendorService(RestTemplate restTemplate, Config appConfig) {
         this.restTemplate = restTemplate;
+        this.appConfig = appConfig;
     }
 
     public VendorEntity fetchBooksFromExternalApi() {
-        return restTemplate.getForObject(EXTERNAL_API_URL, VendorEntity.class);
+        try {
+            logger.info("Fetching data from external API...");
+            VendorEntity vendorEntity = restTemplate.getForObject(appConfig.getApiUrl(), VendorEntity.class);
+            logger.info("Data fetched successfully: {}", vendorEntity);
+            return vendorEntity;
+        } catch (Exception e) {
+            logger.error("Error while fetching data from external API", e);
+            throw e;
+        }
     }
 }
